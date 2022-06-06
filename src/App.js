a97b2673ee74904e88a505032f8a775703974f39
YoujungSon <andn1026@gmail.com>
2022-06-06 09:51:23 +0900

    network

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  React.useEffect(() => {
+    let xhr = new XMLHttpRequest();
+    // 0: 객체는 만들었으나 open을 호출하기 전
+    xhr.open("GET", "http://localhost:5001/sleep_times");
+    // 1: 호출한 후
+    xhr.send();
+    // 2: 보내기
+    // 3: 응답을 기다리는 중
+    // 4: 응답완료 요청을 받아옴.
+    //XMLHttpRequest.readyState 라는 프로퍼티가 변경될 때마다 콜백함수를 호출해준다.
+    //
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        console.log(xhr.responseText);
+      }
+    };
+    xhr.onload = function () {
+      console.log(xhr.responseText);
+    };
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
